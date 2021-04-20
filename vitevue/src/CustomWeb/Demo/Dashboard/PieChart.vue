7cc81969054b82593844bf0a507f196446683c5f
Aaron Gong <aaronjxz@gmail.com>
2021-04-20 08:50:06 +0800

    add multipart example for frontend

--- vitevue/src/CustomWeb/Demo/Dashboard/PieChart.vue
+++ vitevue/src/CustomWeb/Demo/Dashboard/PieChart.vue
@@ -13,31 +13,6 @@ export default {
   setup() {
 
     onMounted(() => {
-      const data = [
-        { genre: 'Sports', sold: 275 },
-        { genre: 'Strategy', sold: 115 },
-        { genre: 'Action', sold: 120 },
-        { genre: 'Shooter', sold: 350 },
-        { genre: 'Other', sold: 150 },
-      ];
-/*
-      // Step 1: Create a Chart instance.
-      const chart = new Chart({
-        container: 'pc1', // Specify chart container ID
-        width: 600, // Specify chart width
-        height: 300, // Specify chart height
-      });
-
-      // Step 2: Load the data.
-      chart.data(data);
-
-      // Step 3: Declare the grammar of graphics, draw column chart.
-      chart.interval().position('genre*sold');
-
-      // Step 4: Render chart.
-      chart.render();
-*/
-/*
       const otherRatio = 6.67 / 100; // Other 的占比
       const otherOffsetAngle = otherRatio * Math.PI; // other 占的角度的一半
       const data = [
@@ -173,7 +148,7 @@ export default {
         });
         canvas.draw();
       }
-*/
+
     })
 
     return {
