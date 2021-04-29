3f43be9d5afce45821f579f995a571ddedb19ece
Aaron Gong <aaronjxz@gmail.com>
2021-04-30 01:10:05 +0800

    add filter to table

--- vitevue/src/CustomWeb/Demo/DemoTable.vue
+++ vitevue/src/CustomWeb/Demo/DemoTable.vue
@@ -2,13 +2,72 @@
   <a-table
     :scroll="table.scroll"
     :columns="table.columns" :data-source="table.dataSource" @change="onChange" rowKey="id"
-  />
+  >
+    <template #filterDropdown="{ setSelectedKeys, selectedKeys, confirm, clearFilters, column }">
+      <div style="padding: 8px">
+        <a-input
+          ref="searchInput"
+          :placeholder="`Search ${column.dataIndex}`"
+          :value="selectedKeys[0]"
+          style="width: 188px; margin-bottom: 8px; display: block"
+          @change="e => setSelectedKeys(e.target.value ? [e.target.value] : [])"
+          @pressEnter="handleSearch(selectedKeys, confirm, column.dataIndex)"
+        />
+        <a-button
+          type="primary"
+          size="small"
+          style="width: 90px; margin-right: 8px"
+          @click="handleSearch(selectedKeys, confirm, column.dataIndex)"
+        >
+          <template #icon><SearchOutlined /></template>
+          Search
+        </a-button>
+        <a-button size="small" style="width: 90px" @click="handleReset(clearFilters)">
+          Reset
+        </a-button>
+      </div>
+    </template>
+    <template #filterIcon="filtered">
+      <search-outlined :style="{ color: filtered ? '#108ee9' : undefined }" />
+    </template>
+    <template #customRender="{ text, column }">
+      <span v-if="searchText && searchedColumn === column.dataIndex">
+        <template
+          v-for="(fragment, i) in text
+            .toString()
+            .split(new RegExp(`(?<=${searchText})|(?=${searchText})`, 'i'))"
+        >
+          <mark
+            v-if="fragment.toLowerCase() === searchText.toLowerCase()"
+            class="highlight"
+            :key="i"
+          >
+            {{ fragment }}
+          </mark>
+          <template v-else>{{ fragment }}</template>
+        </template>
+      </span>
+      <template v-else>
+        {{ text }}
+      </template>
+    </template>
+  </a-table>
 </template>
 <script>
 import { defineComponent, onMounted, reactive, ref } from 'vue';
+import { SearchOutlined } from '@ant-design/icons-vue';
 
 export default defineComponent({
+  components: {
+    SearchOutlined,
+  },
   setup() {
+    const state = reactive({
+      searchText: '',
+      searchedColumn: '',
+    })
+    const searchInput = ref()
+
     const table = reactive({
       scroll: { x: 1200, y: 480 },
       columns: [
@@ -26,12 +85,16 @@ export default defineComponent({
         {
           title: 'Title',
           dataIndex: 'title',
-          filter: true,
+          slots: { filterDropdown: 'filterDropdown', filterIcon: 'filterIcon', customRender: 'customRender' },
+          onFilter: (value, record) => record.title.toString().toLowerCase().includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => visible && setTimeout(() => searchInput.value.focus(), 0)
         },
         {
           title: 'Body',
           dataIndex: 'body',
-          filter: true,
+          slots: { filterDropdown: 'filterDropdown', filterIcon: 'filterIcon', customRender: 'customRender' },
+          onFilter: (value, record) => record.body.toString().toLowerCase().includes(value.toLowerCase()),
+          onFilterDropdownVisibleChange: visible => visible && setTimeout(() => searchInput.value.focus(), 0)
         },
       ],
       dataSource: []
@@ -53,9 +116,26 @@ export default defineComponent({
       console.log('params', pagination, filters, sorter);
     };
 
+    const handleSearch = (selectedKeys, confirm, dataIndex) => {
+      confirm();
+      state.searchText = selectedKeys[0];
+      state.searchedColumn = dataIndex;
+    };
+
+    const handleReset = clearFilters => {
+      clearFilters();
+      state.searchText = '';
+    };
+
     return {
       table,
       onChange,
+
+      handleSearch,
+      handleReset,
+      searchText: '',
+      searchInput,
+      searchedColumn: '',
     };
   },
 });
