a8b5c17b07f53cb2084935ed27d44cb8f9008438
Aaron Gong <aaronjxz@gmail.com>
2021-10-20 09:09:19 +0800

    wip jwt

--- streamlit/jwt-test.py
+++ streamlit/jwt-test.py
@@ -30,6 +30,58 @@ except:
 # if token error or no token, redirect to IDP
 # else pass continue
 
+"""
+# https://pyjwt.readthedocs.io/en/latest/usage.html
+
+pip install pyjwt[crypto]
+
+import jwt
+from cryptography.hazmat.primitives import serialization
+from jwt.exceptions import ExpiredSignatureError
+
+
+# HS256
+payload_data = {
+  "exp": datetime.datetime.now(tz=timezone.utc) + datetime.timedelta(seconds=30)
+  "sub": "4242",
+  "name": "Jessica Temporal",
+  "nickname": "Jess"
+}
+my_secret = 'my_super_secret'
+token = jwt.encode(
+    payload=payload_data,
+    key=my_secret,
+    leeway=10 # or datetime.timedelta(seconds=10)
+)
+
+header_data = jwt.get_unverified_header(token)
+# {'typ': 'JWT', 'alg': 'RS256'}
+
+try:
+  payload = jwt.decode(
+    token,
+    key='my_super_secret',
+    algorithms=[header_data['alg'], ]
+  )
+except ExpiredSignatureError as error:
+  print(f'Unable to decode the token, error: {error}')
+
+
+# RS256
+# read and load the key
+private_key = open('.ssh/id_rsa', 'r').read()
+key = serialization.load_ssh_private_key(private_key.encode(), password=b'')
+new_token = jwt.encode(
+    payload=payload_data,
+    key=key,
+    algorithm='RS256'
+)
+
+public_key = open('.ssh/id_rsa.pub', 'r').read()
+key = serialization.load_ssh_public_key(public_key.encode())
+jwt.decode(jwt=token, key=key, algorithms=['RS256', ])
+"""
+
 def main():
   st.write('hello jwt')
   st.write(st.session_state.token)
