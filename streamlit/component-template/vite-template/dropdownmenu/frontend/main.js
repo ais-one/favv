337b1fe78605992fa3f1dc77e72c587e7df6687d
Aaron Gong <aaronjxz@gmail.com>
2021-10-21 18:21:54 +0800

    wip menu

--- streamlit/component-template/vite-template/dropdownmenu/frontend/main.js
+++ streamlit/component-template/vite-template/dropdownmenu/frontend/main.js
@@ -1,5 +1,75 @@
 import { Streamlit } from "streamlit-component-lib"
 
+const defaultStyle = /*css*/`
+body {
+  margin: 0;
+}
+
+#app {
+  margin: 0;
+}
+
+/* Fixed sidenav, full height */
+.sidenav {
+  font-family: Arial;
+  height: 100%;
+  width: 100%;
+  position: fixed;
+  z-index: 1;
+  top: 0;
+  left: 0;
+  background-color: #111;
+  overflow-x: hidden;
+  /* padding-top: 20px; */
+}
+
+/* Style the sidenav links and the dropdown button */
+.sidenav a, .dropdown-btn {
+  padding: 6px 8px 6px 16px;
+  text-decoration: none;
+  font-size: 20px;
+  color: #818181;
+  display: block;
+  border: none;
+  background: none;
+  width:100%;
+  text-align: left;
+  cursor: pointer;
+  outline: none;
+}
+
+/* On mouse-over */
+.sidenav a:hover, .dropdown-btn:hover {
+  color: #f1f1f1;
+}
+
+/* Main content */
+.main {
+  margin-left: 200px; /* Same as the width of the sidenav */
+  font-size: 20px; /* Increased text to enable scrolling */
+  padding: 0px 10px;
+}
+
+/* Add an active class to the active dropdown button */
+.sidenav a.selected {
+  background-color: green;
+  color: white;
+}
+
+/* Dropdown container (hidden by default). Optional: add a lighter background color and some left padding to change the design of the dropdown content */
+.dropdown-container {
+  display: none;
+  background-color: #262626;
+  padding-left: 8px;
+}
+
+/* Optional: Style the caret down icon */
+.fa-caret-down {
+  float: right;
+  padding-right: 8px;
+}
+`
+
 /*
 <div class="sidenav">
   <a href="#about">About</a>
@@ -36,14 +106,17 @@ function onRender(event) {
   if (rendered) return
   else rendered = true
 
+  const styleTag = document.createElement('style') // hopefully this does not keep getting created...
+  styleTag.innerText = defaultStyle
+  document.head.appendChild(styleTag)
+
   console.log('Render', event)
 
   // TBD create based on table NOSONAR
   const data = event.detail
   const items = data.args["items"]
   let selected = data.args["selected"] || ''
-  const opened = data.args["opened"] || []
-
+  let opened = data.args["opened"] || []
   // const styles = data.args["styles"]
 
   const root = document.createElement('div')
@@ -51,7 +124,7 @@ function onRender(event) {
 
   function setupMenuItem(menuItem) {
     const aTag = document.createElement('a') // to refactor duplicate
-    // TBD set style - background color, text color, bold?, size?
+    if (selected === menuItem.label) aTag.classList.add('selected')
     aTag.onclick = () => {
       selected = menuItem.label // No need to update style
       returnState(selected, opened)
@@ -70,6 +143,13 @@ function onRender(event) {
       const divContainerDd = document.createElement('div')
       divContainerDd.classList.add('dropdown-container')
 
+      if (opened.includes(item.label)) {
+        divContainerDd.style.display = "block"
+        // set open icon
+      } else {
+        // set close icon
+      }
+
       // initialize menu group state
       // TBD set style - background color, text color, bold?, size?
       // set icon expanded or not
@@ -78,9 +158,15 @@ function onRender(event) {
       btnTag.onclick = (e) => {
         const dropdownContent = divContainerDd
         if (dropdownContent.style.display === "block") {
-          dropdownContent.style.display = "none";
+          opened = opened.filter(open => item.label !== open)
+          console.log('aaa1', opened)
+          // set close icom
+          dropdownContent.style.display = "none"
         } else {
-          dropdownContent.style.display = "block";
+          opened.push(item.label)
+          console.log('aaa2', opened)
+          // set open icom
+          dropdownContent.style.display = "block"
         }
       }
       root.appendChild(btnTag)
